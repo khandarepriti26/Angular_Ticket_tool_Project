673b3b1f007a06fc627dccdd74c5bff05277ec4f
priti <pritikhandare1997@gmail.com>
2024-12-27 19:31:52 +0530

    add user

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'my_ticket_tool_project';
+  title = 'ticket_tool_angular_18';
 }
